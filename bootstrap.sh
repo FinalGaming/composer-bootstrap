fbda93559164e94ba1fc7105a583e72d76950866
Daniel Ruf <daniel@daniel-ruf.de>
2017-09-09 23:49:05 +0200

    add path setting

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,6 @@ mkdir bin
 cd bin
 curl -sS https://getcomposer.org/installer | php
 echo "alias composer='${userdir}/bin/composer.phar'" >> ${userdir}/.user_bashrc
+echo "export PATH='$PATH:~/.config/composer/vendor/bin'" >> ${userdir}/.user_bashrc
 source /.bashrc
+composer --version
